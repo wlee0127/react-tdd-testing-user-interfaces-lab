ae392235d609f9234e921b3093a786b6b16de40c
william lee <will.lee.npc@gmail.com>
2024-02-03 11:43:46 -0500

    complete lab

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 function App() {
-  return <div>Your code here</div>;
+  return <div>
+    <h1>hi, i'm your name</h1>
+    <img src="../public/logo192.png" alt="sample"/>
+    <h2>About Me</h2>
+    <p>my bio</p>
+    <a href="link1">link1</a>
+    <a href="link2">link2</a>
+  </div>;
 }
 
 export default App;
--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,6 +1,57 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, getByAltText, getByText } from "@testing-library/react";
 import '@testing-library/jest-dom';
 
 import App from "../App";
 
 // Your tests here
+test("dispays a top level heading witht eh text hi im___", ()=>{
+    render(<App/>);
+    const topLevelHeading = screen.getByRole("heading", {
+        name: /hi, i'm/i,
+        exact: false,
+        level: 1,
+    })
+
+    expect(topLevelHeading).toBeInTheDocument();
+})
+
+test("an image with alt text", ()=>{
+    const {getByAltText}=render(<App/>);
+
+    const imgElement=getByAltText("sample");
+    expect(imgElement).toBeInTheDocument();
+})
+
+test("dispays a top level heading with the text About Me", ()=>{
+    render(<App/>);
+    const topLevelHeading = screen.getByRole("heading", {
+        name: /About Me/i,
+        exact: false,
+        level: 2,
+    })
+
+    expect(topLevelHeading).toBeInTheDocument();
+})
+
+test("paragraph for my bio", ()=>{
+    const {getByText}=render(<App/>);
+    const pElement=getByText("my bio");
+    expect(pElement).toBeInTheDocument();
+})
+
+
+test("link to page 1", ()=>{
+    const {getByRole}=render(<App/>);
+    const linkElement1=getByRole("link",{
+        name:/link1/i
+    });
+    expect(linkElement1).toBeInTheDocument();
+})
+
+test("link to page 2", ()=>{
+    const {getByRole}=render(<App/>);
+    const linkElement2=getByRole("link",{
+        name:/link2/i
+    });
+    expect(linkElement2).toBeInTheDocument();
+})
